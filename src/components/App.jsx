b3158342f293e184c7cdafdb8a3365dfe11eb327
zganz <humanmailer@gmail.com>
2022-07-09 20:25:09 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
-// import {Form} from './Form/Form';
+import {Form} from './Form/Form';
 
 export class App extends React.Component {
   state = {
     contacts: [],
     filter: '',
   };
-}
 
 
-render() {
-  return (
-    <div>
-      <h1>Phonebook</h1>
-      <Form onSubmit={this.formSubmit} />
-      {/* <h2>Contacts</h2>
-      <Filter value={this.state.filter} onChange={this.onChangeFilter} />
-      <ContactList
-        contacts={this.returnFilteredContacts()}
-        onDelete={this.deleteContact}
-      /> */}
-    </div>
-  );
+
+  render() {
+    return <div>
+              <h1>Phonebook</h1>
+              <Form />
+          </div>
+    
+  }
+
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
+import css from './form.module.css';
 
-// export class Form extends React.Component {
-//     state = {
-//       name: '',
-//       number: '',
-//     };
+export class Form extends React.Component {
+  state = {
+    name: '',
+    number: '',
+  };
 
+  render() {
+    return (
+      <form onSubmit={this.onSubmit} className={css.form}>
+        <label>
+          Имя
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label>
+          Номер
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
 
-
-
-
-
-
-// }
-
+        <button type="submit">Добавить контакт</button>
+      </form>
+    );
+  }
+}
