250f4dce4e81804e61af98a8195014bfbf15a7a3
zganz <humanmailer@gmail.com>
2022-07-12 18:34:42 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,12 @@ import { Filter } from './Filter/Filter';
 
 export class App extends React.Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
@@ -25,6 +30,11 @@ export class App extends React.Component {
       : this.state.contacts.filter(({ name }) =>
           name.toLowerCase().includes(this.state.filter.toLowerCase())
         );
+  };
+
+  haveDublicats = (contacts, data) => {
+    const alert = contacts.some(contact => contact.name === data.name);
+    return alert(`${data.name} is already in contacts`);
   };
 
   render() {
